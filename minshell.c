15bf3fab086976372d923c0f916da3cfdef297bc
0l3d <0l3dgit@gmail.com>
2025-07-14 22:08:54 +0300

    Add: Minimal lexer.

--- minshell.c
+++ minshell.c
@@ -1,9 +1,20 @@
 #include <limits.h>
+#include <pty.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <termios.h>
 #include <unistd.h>
+#include <ctype.h>
+// gcc minshell.c -I/usr/include/lua5.4 -L/usr/lib/ -Wl,-Bstatic -llua5.4 -Wl,-Bdynamic -lc -lm -o minshell
+
+
+typedef struct {
+  char* command;
+  char* args[100];
+  int arg_count;
+} Command;
 
 struct termios orig_set, new_set;
 
@@ -12,11 +23,85 @@ void prompt(char *path) {
   fflush(stdout);
 }
 
-void init_shell() {
+void process(char* command, char *output, size_t size) {
+  int amaster[2];
+  pid_t pid;
+  int nbytes;
+
+  pid = forkpty(amaster, NULL, NULL, NULL);
+  if (pid == -1) {
+    fprintf(stderr, "forkpty is failed");
+    return;
+  }
+  
+
+  if (pid == 0) {
+    if (execlp("ls", "ls", NULL) == -1) {
+      perror("Command execution failed: ");
+      return;
+    } else {
+      printf("command is not executed");
+    }
+  } else {
+    size_t total = 0;
+    
+    while ((nbytes = read(amaster[0], output + total, size - total - 1)) > 0) {
+      total += nbytes;
+      printf("%s", output);
+    }
+    output[total] = '\0';
+    waitpid(pid, NULL, 0);
+  }
+}
+
+int command_lexer(char* bufin, char* bufout[], int max_count) {
+  int token_count = 0;
+  const char* p = bufin;
+  while (*p != '\0') {
+    while(isspace(*p)) p++;
+    if (*p == '\0') break;
+
+    // PARSER
+    if (*p == '>' || *p == '<' || *p == '|') {
+      char* sym = malloc(2);
+      sym[0] = *p;
+      sym[1] = '\0';
+      bufout[token_count] = sym;
+      token_count++;
+      p++;
+    } else if (*p == '"' || *p == '\'') {
+      char in_string = *p;
+      const char* string_start = ++p;
+      while (*p && *p != in_string) p++;
+      int stringlen = p - string_start;
+      char* in_string_tokens = malloc(stringlen + 1);
+      strncpy(in_string_tokens, string_start, stringlen);
+      in_string_tokens[stringlen] = '\0';
+      bufout[token_count++] = in_string_tokens;
+      if (*p) p++;
+    } else {
+      const char* word_start = p;
+      while (*p && !isspace(*p) && *p != '>' && *p != '<' && *p != '|') p++;
+      int word_len = p - word_start;
+      char *word = malloc(word_len + 1);
+      strncpy(word, word_start, word_len);
+      word[word_len] = '\0';
+      bufout[token_count++] = word;
+    }
+    if (token_count >= max_count) break;
+  }
+  bufout[token_count] = NULL;
+  return token_count;
+}
+
+
+void interface() {
   tcgetattr(STDIN_FILENO, &orig_set);
   new_set = orig_set;
   new_set.c_lflag &= ~(ICANON | ECHO | ISIG);
   tcsetattr(STDIN_FILENO, TCSANOW, &new_set);
+
+
   char pwd[PATH_MAX];
   if (getcwd(pwd, sizeof(pwd)) == NULL) {
     perror("pwd error");
@@ -53,7 +138,7 @@ void init_shell() {
         continue;
       } else {
         fflush(stdout);
-        system(command);
+        // process(command);
         command_index = 0;
         memset(command, 0, sizeof(command));
         prompt(pwd);
@@ -128,6 +213,20 @@ void init_shell() {
 }
 
 int main() {
-  init_shell();
+  //  interface();
+
+  char input[] = "ls -la > lsout < echo \"finished\"";
+  char* tokens[100];
+
+  int count = command_lexer(input, tokens, 100);
+
+  for (int i = 0; i < count; i++) {
+    printf(": %s\n", tokens[i]);
+  }
+
+  for(int i = 0; i < count; i++) {
+    free(tokens[i]);
+  }
+  
   return 0;
 }
